446d147bb14b17f04e14b94f891b11a0cc4965dd
Miguel <104520808+aldarion-git@users.noreply.github.com>
2023-01-03 13:05:35 +0100

    Add files via upload

--- streamlit/setup.sh
+++ streamlit/setup.sh
@@ -3,9 +3,3 @@ echo "\
 [general]\n\
 email = \"mangel.alb@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
