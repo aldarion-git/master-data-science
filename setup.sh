975265a3dc92a861efb03752dcc7f7f36f782c16
Miguel <104520808+aldarion-git@users.noreply.github.com>
2023-01-03 16:41:32 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "[server]"  > ~/.streamlit/config.toml 
+echo "[server]"  >> ~/.streamlit/config.toml 
 echo "headless = true"  >> ~/.streamlit/config.toml
 echo "port = $PORT"  >> ~/.streamlit/config.toml
 echo "enableCORS = false"  >> ~/.streamlit/config.toml
-echo "[theme]"  > ~/.streamlit/config.toml
+echo "[theme]"  >> ~/.streamlit/config.toml
 echo "primaryColor='#0B0078'"  >> ~/.streamlit/config.toml
 echo "backgroundColor='#FFF'"  >> ~/.streamlit/config.toml
 echo "secondaryBackgroundColor='#F0F2F6'"  >> ~/.streamlit/config.toml
