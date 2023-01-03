5ebb46d4dd5900f97de78b38124c14c54894e2d8
Miguel <104520808+aldarion-git@users.noreply.github.com>
2023-01-03 15:02:42 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-import streamlit as st
-import pandas as pd
-import pickle
-import xgboost as xgb
-from sklearn.metrics.pairwise import cosine_similarity
-import geopandas as gpd
-import plotly.express as px
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-
-
 def main():
   st.write('hello world')
 	
